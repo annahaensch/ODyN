eb1384268caf6cca4c52ffb955afb55330971cb1
Anna Haensch <annahaensch@gmail.com>
2021-12-12 20:31:54 -0500

    add simulation scripts

---
+++ scripts/oregon_simulation.py
@@ -0,0 +1,76 @@
+""" Run Full Oregon Simulations.
+"""
+import sys
+sys.path.append('..')
+
+import src as odyn
+import logging
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+
+    logging.info("Loading AL geographic data.")
+
+    # Load geographic data
+    geo_df = odyn.get_county_mapping_data(county = "Multnomah", state = "OR")
+    hesitancy_dict = odyn.get_hesitancy_dict(geo_df)
+    prob = list(hesitancy_dict.values())
+
+    model = odyn.OpinionNetworkModel(
+                                probabilities = prob,
+                                delta = 36,
+                                beta = 2.5,
+                                include_weight = True,
+                                include_opinion = True,
+                               )
+
+    model.populate_model(num_agents = 1000, 
+                            show_plot = False)
+    initial_belief_df = mode.belief_df.copy()
+
+    # Save population data.
+    model.agent_df.to_parquet("../data/or/multnomah/agent_df.pq")
+    model.belief_df.to_parquet("../data/or/multnomah/belief_df.pq")
+    model.prob_df.to_csv("../data/or/multnomah/prob_df.csv")
+    model.adjacency_df.to_csv("../data/or/multnomah/adjacency_df.csv")
+
+    logging.info("\n Model Loaded.")
+
+    reaches = [0,2,4,6,8,10]
+
+    # Run simulations for no influencers.
+    logging.info("\n Running simulation for no influencers.")
+    
+    model.reach_dict = {0:0,2:0}
+    mega_influencer_df = model.connect_mega_influencers(
+    belief_df = initial_belief_df)
+    model.mega_influencer_df = mega_influencer_df
+
+    sim = odyn.NetworkSimulation()
+    sim.run_simulation(model = model, phases = 60)
+    sim.dynamic_belief_df.to_csv(
+        "../data/or/multnomah/simulation_results_none.csv")
+
+    # Run simulations for left reach 0,.2,.4,.6,.8,1. with right reach .8.
+    for i in range(len(reaches)):
+        r = reaches[i]
+
+        logging.info("\n Running simulation for r = {}".format(r))
+        
+        model.reach_dict = {0:r/10,2:.8}
+        mega_influencer_df = model.connect_mega_influencers(
+        belief_df = initial_belief_df)
+        model.mega_influencer_df = mega_influencer_df
+
+        sim = odyn.NetworkSimulation()
+        sim.run_simulation(model = model, phases = 60, store_results = False)
+        sim.dynamic_belief_df.to_csv(
+            "../data/or/multnomah/simulation_results_{}.csv".format(r))
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
