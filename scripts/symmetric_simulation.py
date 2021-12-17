38bcd3cdc7eb250210e1c992459069036bd5e093
Anna Haensch <annahaensch@gmail.com>
2021-12-16 21:52:14 -0500

    update scripts

--- scripts/symmetric_simulation.py
+++ scripts/symmetric_simulation.py
@@ -25,10 +25,11 @@ def main():
 
     model = odyn.OpinionNetworkModel(
                                 probabilities = [.45,.1,.45],
-                                delta = 5,
-                                beta = 1.7,
+                                distance_scaling_factor = 1/50,
+                                importance_of_weight = 1.3, 
+                                importance_of_distance = 2.7,
                                 include_weight = True,
-                                include_opinion = True,
+                                include_opinion = True
                                )
 
     model.populate_model(num_agents = 1000, 
@@ -38,8 +39,13 @@ def main():
     # Save population data.
     model.agent_df.to_parquet("../data/sym/no_name_county/agent_df.pq")
     model.belief_df.to_parquet("../data/sym/no_name_county/belief_df.pq")
-    model.prob_df.to_csv("../data/sym/no_name_county/prob_df.csv")
-    model.adjacency_df.to_csv("../data/sym/no_name_county/adjacency_df.csv")
+    model.prob_df.columns = [str(i) for i in model.prob_df.columns]
+    model.prob_df.to_parquet("../data/sym/no_name_county/prob_df.pq")
+    model.prob_df.columns = [int(i) for i in model.prob_df.columns]
+
+    model.adjacency_df.columns = [str(i) for i in model.adjacency_df.columns]
+    model.adjacency_df.to_parquet("../data/sym/no_name_county/adjacency_df.pq")
+    model.adjacency_df.columns = [int(i) for i in model.adjacency_df.columns]
 
     logging.info("\n Model Loaded.")
 
@@ -47,15 +53,16 @@ def main():
     # Run simulations for no influencers.
     logging.info("\n Running simulation for no influencers.")
     
-    model.reach_dict = {0:0,2:0}
+    model.left_reach = 0
+    model.right_reach = 0
     mega_influencer_df = model.connect_mega_influencers(
     belief_df = initial_belief_df)
     model.mega_influencer_df = mega_influencer_df
 
     sim = odyn.NetworkSimulation()
     sim.run_simulation(model = model, phases = 60)
-    sim.dynamic_belief_df.to_csv(
-        "../data/sym/no_name_county/simulation_results_none.csv")
+    sim.dynamic_belief_df.to_parquet(
+        "../data/sym/no_name_county/simulation_results_none.pq")
 
     # Run simulations for left reach 0,.2,.4,.6,.8,1. with right reach .8.
     reaches = [0,2,4,6,8,10]
@@ -64,15 +71,17 @@ def main():
 
         logging.info("\n Running simulation for r = {}".format(r))
         
-        model.reach_dict = {0:r/10,2:.8}
+        model.left_reach = r/10
+        model.right_reach = .8
         mega_influencer_df = model.connect_mega_influencers(
         belief_df = initial_belief_df)
         model.mega_influencer_df = mega_influencer_df
 
         sim = odyn.NetworkSimulation()
         sim.run_simulation(model = model, phases = 60, store_results = False)
-        sim.dynamic_belief_df.to_csv(
-            "../data/sym/no_name_county/simulation_results_{}.csv".format(r))
+        sim.dynamic_belief_df.columns = [str(i) for i in sim.dynamic_belief_df.columns]
+        sim.dynamic_belief_df.to_parquet(
+            "../data/sym/no_name_county/simulation_results_{}.pq".format(r))
 
     return None
 
