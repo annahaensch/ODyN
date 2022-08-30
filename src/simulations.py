01759bbbf395989c8c78f489800a99eba3b33bc6
Anna Haensch <annahaensch@gmail.com>
2022-08-30 11:31:44 -0400

    add initial sampling to assign_weights_and_beliefs

--- src/simulations.py
+++ src/simulations.py
@@ -302,13 +302,21 @@ class OpinionNetworkModel(ABC):
         belief_df = agent_df.copy()
         power_law_exponent = self.power_law_exponent
         k = -1/(power_law_exponent)
-        modes = np.linspace(-1,1,len(self.probabilities))
+        
         
         assert np.sum(np.array(self.probabilities)) == 1, "Probabilities must sum to 1."
         
         belief_df["weight"] = np.random.uniform(0,1,belief_df.shape[0]) ** (k)
-        belief_df["belief"] = np.random.choice(modes, belief_df.shape[0], 
-                                p = self.probabilities)
+
+        means = np.linspace(-1,1,len(self.probabilities))
+        components = [i for i in range(len(means))]
+        cov = np.abs((means[1] - means[0])/2)
+        # Choose gmm component
+        gmm_component = np.random.choice(components,belief_df.shape[0], p = self.probabilities)
+        # Sample from gaussians by component
+        belief = [np.random.normal(loc = means[c], scale = cov) for c in gmm_component]
+        belief_df["belief"] = belief
+
         belief_df["decile"] = pd.qcut(belief_df["weight"], q = 100, labels = [
                                 i for i in range(1,101)])
 
